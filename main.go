c11575835c8da35594d985e252d4dada506b2d4d
fabian-ss <randommusicd@gmail.com>
2022-07-28 00:37:04 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"github.com/fabian-ss/GolangBasicSwagger/app/routes"
+	_ "github.com/fabian-ss/GolangBasicSwagger/docs"
+	"github.com/gofiber/fiber/v2"
+)
+
+// @title Api usando Fiber, Swagger y GORM
+// @version 1.0.0
+// @description Api basica usando GORM.
+// @termsOfService http://swagger.io/terms/
+// @contact.name API Support
+// @contact.email your@mail.com
+// @license.name Apache 2.0
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+// @securityDefinitions.apikey ApiKeyAuth
+// @in header
+// @name Authorization
+// @BasePath /api
+func main() {
+	app := fiber.New()
+	routes.SwaggerRoute(app)
+	app.Listen(":8080")
+}
